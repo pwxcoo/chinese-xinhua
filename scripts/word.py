cad702f2fbf4fa28755821772fbbe8525b6771b0
pwxcoo <1050270321@qq.com>
2018-02-10 23:14:48 +0800

    [BUG]修复汉字数据库不完整，添加more多音字

--- scripts/word.py
+++ scripts/word.py
@@ -36,14 +36,17 @@ def downloader(url):
             print(f'{words[i]} is failed!')
             continue
 
-        wordhtml = BeautifulSoup(response.content.decode('gbk', errors='ignore'), "lxml")
-        tr = wordhtml.find_all('table')[5].find_all('tr')
-        explanation = tr[6].find_all('td')[1].text
-        res.append({'word': tr[2].find_all('td')[1].text.strip(),\
-                    'strokes': tr[3].find_all('td')[1].text.strip(),\
-                    'pinyin': tr[4].find_all('td')[1].text.strip(),\
-                    'radicals': tr[5].find_all('td')[1].text.strip(),\
-                    'explanation': explanation[explanation.find('\r\n'):].strip()})
+        wordhtml = BeautifulSoup(response.content.decode('gbk', errors='ignore').replace('<br/>', '\n').replace('<br>', '\n')\
+                     , "lxml")
+        td = wordhtml.find_all('table')[4].find_all('td')
+        explanation = td[12].text.strip()
+        res.append({'word': td[1].text.strip(),\
+                    'oldword': td[4].text.strip(),\
+                    'strokes': td[6].text.strip(),\
+                    'pinyin': td[8].text.strip(),\
+                    'radicals': td[10].text.strip(),\
+                    'explanation': explanation[explanation.find('\r\n'):].strip(),\
+                    'more': td[14].text.strip()})
     return res
 
 if __name__ == '__main__':
